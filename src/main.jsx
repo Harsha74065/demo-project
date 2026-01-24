b6193a15c859401e03b76b2269c37dfc11cad567
HarshaVardhan Naidu <168506673+Harsha74065@users.noreply.github.com>
2026-01-24 17:47:46 +0530

    Remove react-quill CSS import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './App.css'
-import 'react-quill/dist/quill.snow.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
